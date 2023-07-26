9ab950451c0602c8f624cc0cd593e881f62e74c2
uzen-won <daewon2197@gmail.com>
2023-07-26 15:45:28 +0900

    static

---
+++ src/routes/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Home page..</h1>
+  <h2>{{ wonMessage }}</h2>
+</template>
+
+<script>
+export default {
+  computed: {
+    wonMessage() {
+      return this.$store.state.won.message
+    }
+  }
+}
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore } from 'vuex'
+import won from './won'
+
+export default createStore({
+  modules: {
+    won
+  }
+})
